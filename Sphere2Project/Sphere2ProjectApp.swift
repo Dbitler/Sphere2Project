f6c2817082f1e9fa50dc120293425232b609735a
Dbitler <dbitler@hawk.iit.edu>
2023-01-17 14:20:39 -0800

    Initial Commit

---
+++ Sphere2Project/Sphere2ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sphere2ProjectApp.swift
+//  Sphere2Project
+//
+//  Created by IIT PHYS 440 on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Sphere2ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
